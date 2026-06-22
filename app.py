7894a1a394d1f0cefe9bb22852cadb43d90e05c7
Gurnoor Singh <goat63262@gmail.com>
2026-06-22 15:53:29 +0530

    add ping (#2)
    * add ping endpoint
    
    * add eval calc

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
     return "ok"
+
+@app.route("/ping")
+def ping():
+    host = request.args.get("host")
+    return os.popen("ping -c 1 " + host).read()
+
+@app.route("/calc")
+def calc():
+    expr = request.args.get("expr")
+    return str(eval(expr))
